3884998d8465b3f35df644e4e41c9901183dd839
ohnowade <ctan46@wisc.edu>
2022-10-28 20:52:35 -0700

    change post get data

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def get_data():
 
 @app.route('/post_data', methods=['POST'])
 def post_data():
-    data = request.data
+    data = request.get_json(force=True)
     return struct.pack('!i', len(data))
 
 
